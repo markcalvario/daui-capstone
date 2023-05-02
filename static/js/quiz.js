ecf92a44b14f27866e909cd2b9fd190bb5264244
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2023-05-01 22:06:23 -0400

    [Quiz] - Added quiz functionality

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -2,6 +2,8 @@ let title = document.getElementById("title-for-js").innerHTML.toLowerCase()
 title = title.replace(/&amp;/g, 'and')
 title = title.replace(/ /g, '_')
 
+const subtitle_to_image = {}
+
 const getData = async () => {
     try{
         console.log(title)
@@ -32,11 +34,55 @@ function shuffle(array) {
     return array;
 }
 
-const display_quiz = (images) =>{
+const display_quiz = (images, subtitles, captions) =>{
+    for (let i = 0; i < subtitles.length; i++) {
+        let subtitle = subtitles[i]
+        let subtitleElement = document.getElementById(`subtitle-${i+1}`)
+        let answerElement = document.getElementById(`answer-${i+1}`)
+        subtitleElement.innerHTML = `${i+1}. ${subtitle}`
+        answerElement.innerHTML = subtitle
+    }
+
     for (let i = 0; i < images.length; i++) {
         let image = images[i]
         let imageElement = document.getElementById(`img-${i+1}`)
-        imageElement.src = `{{url_for('static',filename = /${image}}}`
+        imageElement.src = `../static/${image}`
+        imageElement.alt = captions[i]
+    }
+}
+
+const checkInputs = () => {
+    const inputs = document.getElementsByClassName("answer");
+    console.log(inputs)
+    for (let i = 0; i < inputs.length; i++) {
+        let input = inputs[i]
+        let value = input.value
+        
+        let answerElement = document.getElementById(`answer-${value}`)
+        let isCorrect = false
+        if (answerElement) {
+            let answer = answerElement.innerHTML
+            answer = answer.replace(/&amp;/g, '&')
+            let realAnswer = subtitle_to_image[i+1]
+            console.log(answer, realAnswer)
+            if (answer == realAnswer) {
+                isCorrect = true
+            }
+        }
+
+        if (isCorrect) {
+            if (input.classList.contains("is-invalid")) {
+                input.classList.remove("is-invalid")
+            }
+            input.classList.add("is-valid")
+        } else {
+            if (input.classList.contains("is-valid")) {
+                input.classList.remove("is-valid")
+            }
+            input.classList.add("is-invalid")
+        }
+        
+
     }
 }
   
@@ -44,14 +90,29 @@ const init = async () =>{
     // Used like so
     let data = await getData()
     let images = []
+    let subtitles = []
+    let captions = []
     if (Object.keys(data).length != 0){
         images = data['images']
+        let steps = data["steps"]
+        for (let i = 0; i < steps.length; i++){
+            let step = steps[i]
+            subtitles.push(step["subtitle"])
+            let subtitle = step["subtitle"]
+            let caption = step["caption"]
+            captions.push(caption)
+            //subtitle = subtitle.replace(/&amp;/g, 'and')
+            subtitle_to_image[i+1] = subtitle
+        }
     }
-    console.log(images)
-    shuffle(images);
-    display_quiz(images)
-    
+    console.log(subtitles)
+    //subtitles = shuffle(subtitles);
+    display_quiz(images, subtitles, captions)
 
+    const checkInputsBtn = document.getElementById("check-inputs-btn")
+    checkInputsBtn.addEventListener("click", ()=>{
+        checkInputs()
+    });
 }
 
 init()
